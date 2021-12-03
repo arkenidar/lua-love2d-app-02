8dd0d0f02a3cbbec5532bcb00dda538f2afc7a56
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2021-12-03 19:41:17 +0100

    better picking system but not yet complete (some cases to fix)

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ return point[1]>=rectangle[1] and
     point[2]<=(rectangle[2]+rectangle[4])
 end  
 
-function rectangle_draw(rectangle)
+function rectangle_grab(rectangle)
 -- Mouse
 local mx=love.mouse.getX()
 local my=love.mouse.getY()
@@ -40,13 +40,42 @@ else
 end
 rectangle[2]=dy+rectangle[2]
 end -- end if
+return rectangle.ppx~=nil
+end -- end function
+
+function rectangle_draw(rectangle)
+local color=rectangle.color
+love.graphics.setColor(color[1],color[2],color[3])
 -- fill rectangle
 love.graphics.rectangle("fill",rectangle[1],rectangle[2],rectangle[3],rectangle[4])
 end
 
-local rectangle1={50,50,100,100}
-local rectangle2={150,150,100,100}
+local rectangle1={50,50,100,100,color={1,0,0}}
+local rectangle2={150,150,100,100,color={1,1,0}}
+local rectangles={rectangle1,rectangle2}
 function love.draw()
+-- grabbing (front-to-back)
+local grabbed
+--[[
+grabbed=rectangle_grab(rectangle2)
+if not grabbed then
+  grabbed=rectangle_grab(rectangle1)
+end
+--]]
+local grabbed
+for i=#rectangles,1,-1 do
+  grabbed=rectangle_grab(rectangles[i])
+  if grabbed then
+    break
+  end
+end
+-- drawing (back-to-front)
+--[[
 rectangle_draw(rectangle1)
 rectangle_draw(rectangle2)
+--]]
+for i=1,#rectangles do
+  rectangle_draw(rectangles[i])
+end
+
 end
